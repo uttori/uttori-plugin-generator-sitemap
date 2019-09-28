c951f21b13bf51b1e718ce6ff1719d019fc79bef
Matthew Callis <matthew.callis@gmail.com>
2019-09-28 15:59:49 -0700

    Update sitemap plugin to spec

--- src/index.js
+++ src/index.js
@@ -1,64 +1,118 @@
-const debug = require('debug')('Uttori.Plugin.SitemapGenerator');
+const debug = require('debug')('Uttori.Plugin.Generator.Sitemap');
 const R = require('ramda');
-const fs = require('fs-extra');
+const { FileUtility } = require('uttori-utilities');
 
 class SitemapGenerator {
-  static register(uttori) {
+  static register(context) {
+    if (!context || !context.hooks || typeof context.hooks.on !== 'function') {
+      throw new Error("Missing event dispatcher in 'context.hooks.on(event, callback)' format.");
+    }
+    context.hooks.on('document-delete', SitemapGenerator.callback); // UttoriWiki
+    context.hooks.on('document-save', SitemapGenerator.callback); // UttoriWiki
+    context.hooks.on('validate-config', SitemapGenerator.validateConfig); // UttoriWiki
+  }
+
+  static defaultConfig() {
     return {
-      'save': SitemapGenerator.callback, // UttoriWiki
-      'delete': SitemapGenerator.callback, // UttoriWiki
-      'validate-config': SitemapGenerator.validateConfig, // UttoriWiki
+      // Sitemap URL (ie https://sfc.fm)
+      base_url: '',
+
+      // Location where the XML sitemap will be written to.
+      directory: '',
+
+      // Sitemap URL Filter
+      url_filters: [],
+
+      // Sitemap XML Header
+      xml_header: '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">',
+
+      // Sitemap XML Footer
+      xml_footer: '</urlset>',
+
+      // Sitemap Filename
+      filename: 'sitemap',
+
+      // Sitemap Fie Extension
+      extension: 'xml',
+
+      // Sitemap default page priority
+      page_priority: '0.80',
+
+      // Sitemap URLs, must be an array.
+      urls: [],
     };
   }
 
   static validateConfig(config, _uttori) {
     debug('Validating config...');
-    if (!Array.isArray(config.sitemap)) {
-      debug('Config Error: `sitemap` should be an array.');
-      throw new Error('sitemap should be an array.');
+    if (!config.sitemap) {
+      debug('Config Error: `sitemap` configuration key is missing.');
+      throw new Error('sitemap configuration key is missing.');
     }
-    if (config.sitemap_url_filter && !Array.isArray(config.sitemap_url_filter)) {
-      debug('Config Error: `sitemap_url_filter` should be an array.');
-      throw new Error('sitemap_url_filter should be an array.');
+    if (!Array.isArray(config.sitemap.urls)) {
+      debug('Config Error: `urls` should be an array of documents.');
+      throw new Error('urls should be an array of documents.');
     }
+    if (config.sitemap.url_filters && !Array.isArray(config.sitemap.url_filters)) {
+      debug('Config Error: `url_filters` should be an array of regular expression url filters.');
+      throw new Error('url_filters should be an array of regular expression url filters.');
+    }
+    if (!config.sitemap.base_url || typeof config.sitemap.base_url !== 'string') {
+      debug('Config Error: `base_url` is required should be an string of your base URL (ie https://domain.tld).');
+      throw new Error('base_url is required should be an string of your base URL (ie https://domain.tld).');
+    }
+    if (!config.sitemap.directory || typeof config.sitemap.directory !== 'string') {
+      debug('Config Error: `directory` is required should be the path to the location you want the sitemap to be writtent to.');
+      throw new Error('directory is required should be the path to the location you want the sitemap to be writtent to.');
+    }
+
     debug('Validated config.');
   }
 
   static async callback(_document, uttori) {
-    debug('Generating Sitemap');
-    const documents = await uttori.getDocuments(['slug', 'createDate', 'updateDate']);
-    const {
-      sitemap,
-      sitemap_url,
-      sitemap_url_filter,
-      sitemap_page_priority,
-      sitemap_header,
-      sitemap_footer,
-      public_dir,
-      sitemap_filename,
-    } = uttori.config;
+    const defaultConfig = SitemapGenerator.defaultConfig();
+    const directory = uttori.config.sitemap.directory || defaultConfig.directory;
+    const filename = uttori.config.sitemap.filename || defaultConfig.filename;
+    const extension = uttori.config.sitemap.extension || defaultConfig.extension;
 
-    // Add all documents to the sitemap
+    const sitemap = await SitemapGenerator.generateSitemap(uttori);
+    FileUtility.writeFileSync(directory, filename, extension, sitemap);
+    return _document;
+  }
+
+  static async generateSitemap(uttori) {
+    debug('Generating Sitemap', uttori.config.sitemap);
+    const defaultConfig = SitemapGenerator.defaultConfig();
+    const urls = uttori.config.sitemap.urls || defaultConfig.urls;
+    const base_url = uttori.config.sitemap.base_url || defaultConfig.base_url;
+    const url_filters = uttori.config.sitemap.url_filters || defaultConfig.url_filters;
+    const page_priority = uttori.config.sitemap.page_priority || defaultConfig.page_priority;
+    const xml_header = uttori.config.sitemap.xml_header || defaultConfig.xml_header;
+    const xml_footer = uttori.config.sitemap.xml_footer || defaultConfig.xml_footer;
+
+    let documents = [];
+    try {
+      documents = await uttori.storageProvider.getQuery("SELECT 'slug', 'createDate', 'updateDate' FROM documents WHERE slug != '' ORDER BY updateDate DESC LIMIT 10000");
+    } catch (error) {
+      /* istanbul ignore next */
+      debug('Error geting documents:', error);
+    }
+
+    // Add all documents to the urls array
     documents.forEach((document) => {
-      /* istanbul ignore next */
-      let lastmod = document.updateDate ? new Date(document.updateDate).toISOString() : '';
-      /* istanbul ignore next */
-      if (!lastmod) {
-        lastmod = document.createDate ? new Date(document.createDate).toISOString() : '';
-      }
-      sitemap.push({
+      urls.push({
         url: `/${document.slug}`,
-        lastmod,
-        priority: sitemap_page_priority,
+        lastmod: document.updateDate ? new Date(document.updateDate).toISOString() : new Date(document.createDate).toISOString(),
+        priority: page_priority,
       });
     });
 
     let urlFilter = R.identity;
     /* istanbul ignore else */
-    if (Array.isArray(sitemap_url_filter) && sitemap_url_filter.length > 0) {
+    if (Array.isArray(url_filters) && url_filters.length > 0) {
       urlFilter = (route) => {
         let pass = true;
-        sitemap_url_filter.forEach((url_filter) => {
+        url_filters.forEach((url_filter) => {
           try {
             if (url_filter.test(route.url)) {
               pass = false;
@@ -72,9 +126,9 @@ class SitemapGenerator {
       };
     }
 
-    const data = sitemap.reduce((accumulator, route) => {
+    const data = urls.reduce((accumulator, route) => {
       if (urlFilter(route)) {
-        accumulator += `<url><loc>${sitemap_url}${route.url}</loc>`;
+        accumulator += `<url><loc>${base_url}${route.url}</loc>`;
         /* istanbul ignore else */
         if (route.lastmod) {
           accumulator += `<lastmod>${route.lastmod}</lastmod>`;
@@ -92,15 +146,7 @@ class SitemapGenerator {
       return accumulator;
     }, '');
 
-    const output = `${sitemap_header}${data}${sitemap_footer}`;
-    try {
-      await fs.outputFile(`${public_dir}/${sitemap_filename}`, output);
-    } catch (error) {
-      /* istanbul ignore next */
-      debug('Error Generating Sitemap:', error);
-    }
-
-    return input;
+    return `${xml_header}${data}${xml_footer}`;
   }
 }
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,94 +1,166 @@
 /* eslint-disable security/detect-non-literal-fs-filename */
-const fs = require('fs-extra');
 const test = require('ava');
+const { FileUtility } = require('uttori-utilities');
+const SitemapGenerator = require('../src');
 
-const UttoriSitemap = require('../app/sitemap');
-
-const { config, cleanup } = require('./_helpers/server.js');
-
-const documents = () => [
-  {
-    updateDate: new Date('2019-04-20').toISOString(),
-    createDate: new Date('2019-04-20').toISOString(),
-    slug: 'good-title',
+const config = SitemapGenerator.defaultConfig();
+const context = {
+  config: {
+    sitemap: {
+      ...config,
+      base_url: 'https://domain.tld',
+      directory: './',
+    },
   },
-  {
-    updateDate: new Date('2019-04-21').toISOString(),
-    createDate: new Date('2019-04-21').toISOString(),
-    slug: 'fake-title',
+  storageProvider: {
+    getQuery: (_query) => [
+      {
+        updateDate: new Date('2019-04-20').toISOString(),
+        createDate: new Date('2019-04-20').toISOString(),
+        slug: 'good-title',
+      },
+      {
+        updateDate: new Date('2019-04-21').toISOString(),
+        createDate: new Date('2019-04-21').toISOString(),
+        slug: 'fake-title',
+      },
+    ],
   },
-];
+};
 
-test.before(async () => {
-  await cleanup();
+test('SitemapGenerator.register(context): can register', (t) => {
+  t.notThrows(() => {
+    SitemapGenerator.register({ hooks: { on: () => {} } });
+  });
 });
 
-test.after(async () => {
-  await cleanup();
+test('SitemapGenerator.register(context): errors without event dispatcher', (t) => {
+  t.throws(() => {
+    SitemapGenerator.register({ hooks: {} });
+  }, 'Missing event dispatcher in \'context.hooks.on(event, callback)\' format.');
 });
 
-test.afterEach(async () => {
-  await cleanup();
+test('SitemapGenerator.defaultConfig(): can return a default config', (t) => {
+  t.notThrows(SitemapGenerator.defaultConfig);
 });
 
-test('generateSitemapXML(): generates a valid sitemap', (t) => {
+test('SitemapGenerator.generateSitemap(_document, context): generates a valid sitemap', async (t) => {
   t.plan(1);
-  const output = UttoriSitemap.generateSitemapXML({ ...config, sitemap: [] }, documents());
-  t.is(output, '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"><url><loc>/good-title</loc><lastmod>2019-04-20T00:00:00.000Z</lastmod><priority>0.80</priority></url><url><loc>/fake-title</loc><lastmod>2019-04-21T00:00:00.000Z</lastmod><priority>0.80</priority></url></urlset>');
+  const output = await SitemapGenerator.generateSitemap({ ...context, config: { ...context.config, urls: [] } });
+  t.is(output, '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"><url><loc>https://domain.tld/good-title</loc><lastmod>2019-04-20T00:00:00.000Z</lastmod><priority>0.80</priority></url><url><loc>https://domain.tld/fake-title</loc><lastmod>2019-04-21T00:00:00.000Z</lastmod><priority>0.80</priority></url></urlset>');
 });
 
-test('generateSitemapXML(): generates a valid sitemap with no documents', (t) => {
+test('SitemapGenerator.generateSitemap(_document, context): generates a valid sitemap with no documents', async (t) => {
   t.plan(1);
-  const output = UttoriSitemap.generateSitemapXML({ ...config, sitemap: [] });
+  const output = await SitemapGenerator.generateSitemap({ storageProvider: { getQuery: () => [] }, config: { ...context.config, sitemap: { ...context.config.sitemap, urls: [] } } });
   t.is(output, '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"></urlset>');
 });
 
-test('generateSitemapXML(): filters out urls with a filter', (t) => {
+test('SitemapGenerator.generateSitemap(_document, uttori): filters out urls with a filter', async (t) => {
   t.plan(1);
-  const output = UttoriSitemap.generateSitemapXML({ ...config, sitemap: [], sitemap_url_filter: [/fake-title$/i] }, documents());
-  t.is(output, '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"><url><loc>/good-title</loc><lastmod>2019-04-20T00:00:00.000Z</lastmod><priority>0.80</priority></url></urlset>');
+  const output = await SitemapGenerator.generateSitemap({ ...context, config: { ...context.config, sitemap: { ...context.config.sitemap, urls: [], url_filters: [/fake-title$/i] } } });
+  t.is(output, '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"><url><loc>https://domain.tld/good-title</loc><lastmod>2019-04-20T00:00:00.000Z</lastmod><priority>0.80</priority></url></urlset>');
 });
 
-test('UttoriSitemap.generateSitemap(): writes a valid sitemap', async (t) => {
-  t.plan(1);
-
-  await UttoriSitemap.generateSitemap(config, documents());
-  const data = await fs.readFile(`${config.public_dir}/${config.sitemap_filename}`, 'utf8');
-  t.is(data.length, 764);
-});
-
-test('UttoriSitemap.generateSitemap(): writes a valid sitemap with no documents', async (t) => {
-  t.plan(1);
-
-  await UttoriSitemap.generateSitemap(config);
-  const data = await fs.readFile(`${config.public_dir}/${config.sitemap_filename}`, 'utf8');
-  t.is(data.length, 764);
-});
-
-test('validateConfig(config): throws when incorrect sitemap', (t) => {
+test('SitemapGenerator.validateConfig(config, _uttori): throws when sitemaps key is missing', (t) => {
   const error = t.throws(() => {
-    const _uttori = new UttoriWiki({
-      AnalyticsProvider,
-      SearchProvider,
-      StorageProvider,
-      UploadProvider,
-      Renderer,
-      sitemap: {},
-    }, server);
+    SitemapGenerator.validateConfig({});
   }, Error);
-  t.is(error.message, 'sitemap should be an array.');
+  t.is(error.message, 'sitemap configuration key is missing.');
 });
 
-test('validateConfig(config): throws when incorrect sitemap_url_filter', (t) => {
+test('SitemapGenerator.validateConfig(config, _uttori): throws when urls is not an array', (t) => {
   const error = t.throws(() => {
-    const _uttori = new UttoriWiki({
-      AnalyticsProvider,
-      SearchProvider,
-      StorageProvider,
-      UploadProvider,
-      Renderer,
-      sitemap_url_filter: {},
-    }, server);
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: {},
+        url_filters: [],
+      },
+    });
   }, Error);
-  t.is(error.message, 'sitemap_url_filter should be an array.');
+  t.is(error.message, 'urls should be an array of documents.');
+});
+
+test('SitemapGenerator.validateConfig(config, _uttori): throws when url_filters is not an array', (t) => {
+  const error = t.throws(() => {
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: [],
+        url_filters: {},
+      },
+    });
+  }, Error);
+  t.is(error.message, 'url_filters should be an array of regular expression url filters.');
+});
+
+test('SitemapGenerator.validateConfig(config, _uttori): throws when base_url is missing', (t) => {
+  const error = t.throws(() => {
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: [],
+        url_filters: [],
+      },
+    });
+  }, Error);
+  t.is(error.message, 'base_url is required should be an string of your base URL (ie https://domain.tld).');
+});
+
+test('SitemapGenerator.validateConfig(config, _uttori): throws when base_url is not a string', (t) => {
+  const error = t.throws(() => {
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: [],
+        url_filters: [],
+        base_url: null,
+      },
+    });
+  }, Error);
+  t.is(error.message, 'base_url is required should be an string of your base URL (ie https://domain.tld).');
+});
+
+test('SitemapGenerator.validateConfig(config, _uttori): throws when directory is missing', (t) => {
+  const error = t.throws(() => {
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: [],
+        url_filters: [],
+        base_url: 'https://domain.tld',
+      },
+    });
+  }, Error);
+  t.is(error.message, 'directory is required should be the path to the location you want the sitemap to be writtent to.');
+});
+
+test('SitemapGenerator.validateConfig(config, _uttori): throws when directory is not a string', (t) => {
+  const error = t.throws(() => {
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: [],
+        url_filters: [],
+        base_url: 'https://domain.tld',
+        directory: null,
+      },
+    });
+  }, Error);
+  t.is(error.message, 'directory is required should be the path to the location you want the sitemap to be writtent to.');
+});
+
+test('SitemapGenerator.validateConfig(config, _uttori): can validate', (t) => {
+  t.notThrows(() => {
+    SitemapGenerator.validateConfig({
+      sitemap: {
+        urls: [],
+        url_filters: [],
+        base_url: 'https://domain.tld',
+        directory: './',
+      },
+    });
+  });
+});
+
+test('SitemapGenerator.callback(_document, uttori): writes properly generated sitemap to desired location', async (t) => {
+  await SitemapGenerator.callback(null, { ...context, config: { ...context.config, urls: [] } });
+  const output = await FileUtility.readFile('./', 'sitemap', 'xml');
+  await FileUtility.deleteFile('./', 'sitemap', 'xml');
+  t.is(output.length, 758);
 });
